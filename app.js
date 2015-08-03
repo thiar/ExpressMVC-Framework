66eff74a5e029c886fda4f2f8a401aae4a3f42e3
thiar <thiar.hasbiya@gmail.com>
2015-08-03 10:16:55 +0700

    add express-session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var session = require('express-session');
 // var routes = require('./routes/index');
 // var users = require('./routes/users');
 
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'pikachu'}));
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
